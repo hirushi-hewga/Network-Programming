3835d2fa0265a6ff8222593e98350f52bae5f35a
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-17 12:43:14 -0800

    Add project files.

---
+++ sync_client (Sockets)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sync_client__Sockets_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ sync_server (Sockets)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sync_server__Sockets_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
