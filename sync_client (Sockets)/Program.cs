43839e027e2a7676f2639909142af054235083a5
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-17 14:24:56 -0800

    Sockets.UdpClient

--- sync_client (Sockets)/Program.cs
+++ sync_client (Sockets)/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace sync_client__Sockets_
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+
+namespace sync_client__Sockets_
 {
-    internal class Program
+    class Program
     {
+        // адрес и порт сервера, к которому будем подключаться
+        static string address = "127.0.0.1"; // адрес сервера
+        static int port = 8080;             // порт сервера 1000....60 000
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+                //IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+                // IP4 samples: 123.5.6.3    0.0.255.255    10.7.123.184
+                //Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+
+                UdpClient client = new UdpClient();
+                string message = "";
+                while (message != "end")
+                {
+                    Console.Write("Enter a message: ");
+                    message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    //socket.SendTo(data, ipPoint);
+                    
+                    
+                    client.Send(data, data.Length, ipPoint);
+                    // при використанні UDP протоколу, Connect() лише встановлює дані для відправки
+                    // socket.Connect(ipPoint);
+                    // socket.Send(data);
+
+
+                    //получаем ответ получаем сообщение
+                    int bytes = 0;
+                    string response = "";
+                    data = new byte[1024]; // 1KB
+                    //do
+                    //{
+                    //    bytes = socket.ReceiveFrom(data, ref remoteIpPoint);
+                    //    response += Encoding.Unicode.GetString(data, 0, bytes);
+                    //} while (socket.Available > 0);
+
+                    data = client.Receive(ref remoteIpPoint);
+                    response = Encoding.Unicode.GetString(data);
+
+                    Console.WriteLine("server response: " + response);
+                }
+                // закрываем сокет
+                //socket.Shutdown(SocketShutdown.Both);
+                //socket.Close();
+                client.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- sync_server (Sockets)/Program.cs
+++ sync_server (Sockets)/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace sync_server__Sockets_
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace sync_server__Sockets_
 {
-    internal class Program
+    class Program
     {
+        static string address = "127.0.0.1"; // поточний адрес
+        static int port = 8080;              // порт для приема входящих запросов
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            // об'єкт для отримання адреси відправника
+            //IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            // создаем сокет
+            //Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            // связываем сокет с локальной точкой, по которой будем принимать данные
+            UdpClient listener = new UdpClient(ipPoint);
+
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                //listenSocket.Bind(ipPoint);
+                Console.WriteLine("Server started! Waiting for connection...");
+
+                while (true)
+                {
+                    // получаем сообщение
+                    //int bytes = 0;
+                    //byte[] data = new byte[1024];
+                    ///bytes = listenSocket.ReceiveFrom(data, ref remoteEndPoint);//freez
+                    byte[] data = listener.Receive(ref remoteEndPoint);
+
+                    string msg = Encoding.Unicode.GetString(data);
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {msg} from {remoteEndPoint}");
+
+                    // отправляем ответ
+                    string message = "Message was send!";
+                    data = Encoding.Unicode.GetBytes(message);
+                    //listenSocket.SendTo(data, remoteEndPoint);
+
+                    listener.Send(data, data.Length, remoteEndPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //listenSocket.Shutdown(SocketShutdown.Both);
+            //listenSocket.Close();
+            listener.Close();
         }
     }
 }
